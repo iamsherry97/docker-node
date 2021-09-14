0bbd29710bbc8cb4e586d575f3125f175bff1b0c
Shaharyar <engr.shaharyarahmed@gmail.com>
2021-09-15 00:19:52 +0500

    added codedeploy steps

---
+++ scripts/codedeploy/docker-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 3000 489994096722.dkr.ecr.us-west-2.amazonaws.com/nodeapp:latest
---
+++ scripts/codedeploy/pull-image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker pull 489994096722.dkr.ecr.us-west-2.amazonaws.com/nodeapp:latest
